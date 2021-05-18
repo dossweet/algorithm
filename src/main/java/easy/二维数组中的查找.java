ef472d58ec9abd25fae3b7bdb988d51d147927c9
何甜甜 <1731098360@qq.com>
2021-05-18 21:50:39 +0800

    二维数组中的查找的优化版本

--- src/main/java/easy/二维数组中的查找.java
+++ src/main/java/easy/二维数组中的查找.java
@@ -22,4 +22,27 @@ public class 二维数组中的查找 {
         }
         return ifExist;
     }
+
+    public boolean FindBest(int target, int[][] array) {//优化版本
+        boolean ifExist = false;
+        if (array[0].length > 0) {//数组判空
+            int rowLength = array[0].length;
+            for (int i = array.length - 1; i >= 0; i--) {
+                if (target == array[i][0]) {
+                    ifExist = true;
+                    break;
+                } else if (target > array[i][0]) {//右移判断
+                    for (int j = 1; j < rowLength; j++) {
+                        if (target == array[i][j]) {
+                            ifExist = true;
+                            break;
+                        }
+                    }
+                } else if (target < array[i][0]) {//上移判断
+                    continue;
+                }
+            }
+        }
+        return ifExist;
+    }
 }
