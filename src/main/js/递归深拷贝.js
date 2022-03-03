271bd39c4921d7093b8a966dd7dd4e539253881b
何甜甜 <1731098360@qq.com>
2022-03-03 20:41:35 +0800

    更新js版本的递归深拷贝

--- src/main/js/递归深拷贝.js
+++ src/main/js/递归深拷贝.js
@@ -11,7 +11,12 @@ const obj = {
             age: 18,
             sex: '男'
         }
-    ]
+    ],
+    test: {
+        name: 'hello',
+        age: 18,
+        sex: '女'
+    }
 }
 
 const obj01 = ['sweet', ['hello', 'world']];
@@ -19,8 +24,10 @@ const obj01 = ['sweet', ['hello', 'world']];
 function deepCopy(obj) {
     let ret = obj instanceof Array ? [] : {};
     for (let i in obj) {
-        if (i instanceof Array) {
-            deepCopy(obj[i])
+        if (obj[i] instanceof Array || obj[i] instanceof Object) {
+        // if (i instanceof Array || i instanceof Object) {
+            // deepCopy(obj[i]);// 这里记得把值给赋值给ret[i]呀
+            ret[i] = deepCopy(obj[i]);
         } else {
             ret[i] = obj[i];
         }
