3b0939fc91500a24f92c2105339122148bcd9fbb
何甜甜 <1731098360@qq.com>
2022-03-17 21:26:56 +0800

    更新js版本new一个对象

--- src/main/js/js来new一个对象的过程.js
+++ src/main/js/js来new一个对象的过程.js
@@ -2,8 +2,10 @@ function main(name){
     this.name = name;
 }
 
-// 首先声明该构造函数的对象
-let a = new main('hello');
+// 首先声明一个空对象
+let a = {};
+// 下面这个写法直接写出了结果。。并不是new一个对象的过程
+// let a = new main('hello');
 // 把该对象的_proto_绑定到构造函数的prototype上
 a._proto_ = main.prototype;
 // 修改this指向为当前对象
