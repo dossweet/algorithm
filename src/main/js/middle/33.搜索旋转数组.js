0a51db97e0bbd460167b49b7091f5ffa7ee0c252
DoubleSweet <38655923+doublesweet01@users.noreply.github.com>
2022-08-25 19:17:31 +0800

    Create 33.搜索旋转数组.js

---
+++ src/main/js/middle/33.搜索旋转数组.js
@@ -0,0 +1,26 @@
+const search = function (nums, target){
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right){
+        let mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] === target){
+            return mid;
+        }
+        if (nums[mid] >= nums[left]){
+            if (target >= nums[left] && target < nums[mid]){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }else{
+            if (target >= nums[mid] && target <= nums[right]){
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+console.log(search([1, 3], 0));
